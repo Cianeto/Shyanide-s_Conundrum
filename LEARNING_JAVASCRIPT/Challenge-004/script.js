722d9f1dea1c3fe484d9798fb112ad8904bb6b5a
Cianeto <gbdopu02@gmail.com>
2024-07-13 15:46:54 -0300

    GOING CRAZY WITH REGEX

--- LEARNING_JAVASCRIPT/Challenge-004/script.js
+++ LEARNING_JAVASCRIPT/Challenge-004/script.js
@@ -3,7 +3,7 @@ const sortBtn = document.getElementById('sort');
 const display = document.getElementById('display');
 
 arrField.addEventListener('input', () => {
-	onlyNumbers(arrField);
+	theRegexer(arrField);
 });
 
 sortBtn.addEventListener('click', () => {
@@ -11,14 +11,28 @@ sortBtn.addEventListener('click', () => {
 });
 
 function sortArray(arr) {
-
 	display.textContent = 'something';
 }
 
-function onlyNumbers(input) {
-	const regex = input.value.replace(/[^\d.,[\]]/g, '');
+function theRegexer(input) {
+	let str = input.value;
+	str = allowedCharacters(str);
+	str = ensureBrackets(str);
+	//str = regex.replace(/ /, '');
 
-
-	input.value = regex;
+	input.value = str;
 }
-// here i want to 
+
+// /(-?\d+(?:\.\d+)?)/g regex for capturing each value of an array
+
+function ensureBrackets(str) {
+	str = str.replace(/^(?!\[)(.*)/, '[$1');
+	str = str.replace(/(.*)(?<!\])$/, '$1]');
+	str = str.replace(/(\[.*)\[(.*)/, '$1$2');
+	str = str.replace(/(.*)\](.*\])/, '$1$2');
+	return str;
+}
+
+function allowedCharacters(str) {
+	return str.replace(/[^\d., \-\[\]]/g, '');
+}
