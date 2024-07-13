2566222407644e32de0afec971539887d9a88619
Cianeto <gbdopu02@gmail.com>
2024-07-13 17:16:53 -0300

    the regexer master

--- LEARNING_JAVASCRIPT/Challenge-004/script.js
+++ LEARNING_JAVASCRIPT/Challenge-004/script.js
@@ -7,23 +7,33 @@ arrField.addEventListener('input', () => {
 });
 
 sortBtn.addEventListener('click', () => {
-	sortArray();
+	sortArray(arrField.value);
 });
 
-function sortArray(arr) {
-	display.textContent = 'something';
+function sortArray(str) {
+	str = str.replace(/[\[\]]/g, '');
+	let arr = str.match(/(-?\d+(?:\.\d+)?)/g).map(Number);
+	let i, j, temp;
+	for (i = 0; i < arr.length - 2; i++) {
+		for (j = arr.length - 1; j > i; j--) {
+
+		}
+	}
+
+	display.textContent = arr;
 }
 
 function theRegexer(input) {
 	let str = input.value;
 	str = allowedCharacters(str);
 	str = ensureBrackets(str);
-	//str = regex.replace(/ /, '');
-
+	str = separateValues(str);
 	input.value = str;
 }
 
-// /(-?\d+(?:\.\d+)?)/g regex for capturing each value of an array
+function allowedCharacters(str) {
+	return str.replace(/[^\d., \-\[\]]/g, '');
+}
 
 function ensureBrackets(str) {
 	str = str.replace(/^(?!\[)(.*)/, '[$1');
@@ -33,6 +43,16 @@ function ensureBrackets(str) {
 	return str;
 }
 
-function allowedCharacters(str) {
-	return str.replace(/[^\d., \-\[\]]/g, '');
+function separateValues(str) {
+	str = str.replace(/(?<!,)( )/g, '');
+	str = str.replace(/(.*),(?! )(.*)/g, '$1, $2');
+	str = str
+		.split(', ')
+		.map((segment) => {
+			segment = segment.replace(/(-.*)-/g, '$1');
+			segment = segment.replace(/(\..*)\./g, '$1');
+			return segment;
+		})
+		.join(', ');
+	return str;
 }
