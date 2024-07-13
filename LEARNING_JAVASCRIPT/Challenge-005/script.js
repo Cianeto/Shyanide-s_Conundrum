9b74f3083565eb5a7292be5d7e76efcbc158ff0f
Cianeto <gbdopu02@gmail.com>
2024-07-13 18:33:16 -0300

    cha-005 finished

--- LEARNING_JAVASCRIPT/Challenge-005/script.js
+++ LEARNING_JAVASCRIPT/Challenge-005/script.js
@@ -1,3 +1,11 @@
 const input = document.getElementById('field');
 const display = document.getElementById('display');
 
+input.addEventListener('input', ()=>{
+  showVowels(input.value);
+})
+
+function showVowels(input){
+  input = input.replace(/[^aeiou]/gi, '');
+  display.textContent = input;
+}
