458528d0dd723e42120c5c50301eb96ba72664bb
Cianeto <gbdopu02@gmail.com>
2024-07-13 17:47:25 -0300

    cha-004 finished

--- LEARNING_JAVASCRIPT/Challenge-004/script.js
+++ LEARNING_JAVASCRIPT/Challenge-004/script.js
@@ -1,6 +1,8 @@
 const arrField = document.getElementById('array');
 const sortBtn = document.getElementById('sort');
 const display = document.getElementById('display');
+const sortModeBtn = document.getElementById('sortMode');
+let sortMode = 'Ascending';
 
 arrField.addEventListener('input', () => {
 	theRegexer(arrField);
@@ -10,21 +12,44 @@ sortBtn.addEventListener('click', () => {
 	sortArray(arrField.value);
 });
 
+sortModeBtn.addEventListener('click', changeSortMode);
+
 function sortArray(str) {
 	str = str.replace(/[\[\]]/g, '');
 	let arr = str.match(/(-?\d+(?:\.\d+)?)/g).map(Number);
-	let i, j, temp;
+	let i, j;
 	for (i = 0; i < arr.length - 1; i++) {
 		for (j = arr.length - 1; j > i; j--) {
-			if (arr[i] > arr[j]) {
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
+			arr = sortMode === 'Ascending' ? ascending(arr, i, j) : descending(arr, i, j);
 		}
 	}
 
 	display.textContent = '[' + arr.join(', ') + ']';
+}
+
+function changeSortMode() {
+	sortMode = sortMode === 'Ascending' ? 'Descending' : 'Ascending';
+	sortModeBtn.textContent = sortMode;
+}
+
+function ascending(arr, i, j) {
+	let temp;
+	if (arr[i] > arr[j]) {
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	return arr;
+}
+
+function descending(arr, i, j) {
+	let temp;
+	if (arr[i] < arr[j]) {
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	return arr;
 }
 
 function theRegexer(input) {
@@ -49,6 +74,7 @@ function ensureBrackets(str) {
 
 function separateValues(str) {
 	str = str.replace(/(?<!,)( )/g, '');
+	str = str.replace(/(?<!, )(-)/g, '');
 	str = str.replace(/(.*),(?! )(.*)/g, '$1, $2');
 	str = str
 		.split(', ')
